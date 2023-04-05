cdfa23f484e590a73c8833aefcdccd51e3b482e0
Mohamed <ezzaouia.mohamed@gmail.com>
2023-04-05 09:23:13 +0200

    svelte todoapp

---
+++ svelte-todoapp/src/components/AddTask.svelte
@@ -0,0 +1,17 @@
+<script>
+  export let addTask;
+
+  let input;
+</script>
+
+<!-- Todo -->
+<input
+  type="text"
+  placeholder="Ajouter un todo"
+  size="100"
+  onChange={(e) => {
+    input = e.target.value;
+  }}
+/>
+<!-- Todo -->
+<button onClick={() => addTask(input)}>Add Todo</button>
---
+++ svelte-todoapp/src/components/Task.svelte
@@ -0,0 +1,17 @@
+<script>
+  export let index;
+  export let changeDone;
+  export let task;
+
+  // Todo
+  let status = task.done ? "done" : "todo";
+</script>
+
+<input id={index} type="checkbox" on:change={() => changeDone(index)} />
+<label class={status} for={index}>{task.text}</label>
+
+<style>
+  .done {
+    text-decoration: line-through;
+  }
+</style>
